964dd8d02cff25e986310bab8f2f80974910d663
9CD1KC2\admin <smithjrs22@gmail.com>
2022-05-05 15:13:13 -0400

    minor change

--- Assignment/src/TakeABreak.java
+++ Assignment/src/TakeABreak.java
@@ -4,7 +4,7 @@ public class TakeABreak {
     public static void main(String[] args) throws Exception {
     Scanner input = new Scanner(System.in);
         while (true) {
-            System.out.println("Do you want to leave?");
+            System.out.println("Do you want to take a break?");
             String userInput = input.nextLine();
             if (userInput.equals("yes")) {
                 break;
